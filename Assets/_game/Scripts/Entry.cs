b4de91c5d0464d6dc1e854c06d5068d0cd9dd612
Leo Toivio <leo@toivio.se>
2019-07-19 16:17:39 +0200

    Character create

--- Assets/_game/Scripts/Entry.cs
+++ Assets/_game/Scripts/Entry.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Entry : MonoBehaviour
 {
+    List<Character> characters;
+
+    CharacterMovementSystem characterMovementSystem;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characters = new List<Character>(32);
+        characterMovementSystem = new CharacterMovementSystem(characters);
+    }
+
+    private void FixedUpdate()
+    {
+        characterMovementSystem.FixedTick();
     }
 
     // Update is called once per frame
