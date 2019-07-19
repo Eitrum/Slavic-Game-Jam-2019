0d8c133f03b92c0647ff6bdf06ec69d0d71900d0
Björn Englesson <benglesson@gmail.com>
2019-07-19 17:49:25 +0200

    Working on making it possible to fire seeds from player.

---
+++ Assets/_game/Scripts/PlayerInputSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputSystem
+{
+    private readonly List<Character> characters;
+    private readonly List<ShootIntent> fireSeedIntents;
+
+    public PlayerInputSystem(
+        List<Character> characters
+    )
+    {
+        this.characters = characters;
+    }
+
+    public void Tick()
+    {
+        foreach (var character in characters)
+        {
+            if (character.shootIntent)
+            {
+                Transform charTransform = character.transform;
+                fireSeedIntents.Add(new ShootIntent
+                {
+                    position = charTransform.position + charTransform.forward,
+                    direction = charTransform.forward
+                });
+            }
+        }
+    }
+}
