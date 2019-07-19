ded254f91875997e7e663e763ffc6b7328e75af0
Leo Toivio <leo@toivio.se>
2019-07-19 18:41:00 +0200

    Push back with shots

--- Assets/_game/Scripts/Seed.cs
+++ Assets/_game/Scripts/Seed.cs
@@ -4,19 +4,43 @@ using System.Collections.Generic;
 public sealed class Seed : MonoBehaviour
 {
     public Rigidbody rb;
-    public List<SeedImpact> seedImpactQueue;
+    public List<SeedTerrainImpact> seedTerrainImpactQueue;
+    public List<SeedPlayerImpact> seedPlayerImpactQueue;
 
     public void OnCollisionEnter(Collision collision)
     {
-        seedImpactQueue.Add(new SeedImpact
+        Rigidbody otherRb = collision.collider.attachedRigidbody;
+        Character character = null;
+        if (otherRb != null)
         {
-            position = transform.position
-        });
+            character = otherRb.GetComponent<Character>();
+        }
+        if (otherRb != null && character != null)
+        {
+            seedPlayerImpactQueue.Add(new SeedPlayerImpact
+            {
+                position = transform.position,
+                character = character
+            });
+        }
+        else
+        {
+            seedTerrainImpactQueue.Add(new SeedTerrainImpact
+            {
+                position = transform.position
+            });
+        }
         Destroy(this.gameObject);
     }
 }
 
-public struct SeedImpact
+public struct SeedTerrainImpact
 {
     public Vector3 position;
+}
+
+public struct SeedPlayerImpact
+{
+    public Vector3 position;
+    public Character character;
 }
