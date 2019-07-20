9caa0a51a06bcf90c4dd491c09a94c0c279d99a9
Lunaea_Eitrum <sebbbe97@hotmail.com>
2019-07-20 15:50:51 +0200

    Small chnages

---
+++ Assets/_game/Scripts/AutoDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour {
+    public float duration = 2f;
+
+    void Update() {
+        duration -= Time.deltaTime;
+        if(duration <= 0f) {
+            Destroy(gameObject);
+        }
+    }
+}
